4f6508e0bc2bb90dcb869b69c1e61bb6717006ca
this-guy-code-s <codingviewdmv@gmail.com>
2020-04-09 00:48:53 -0400

    i guess this is stretch?...or mvp, i feel ok with this spec

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,29 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_,{input},context){
+      return  context.models.Pet.findMany(input)
+    },
+      apet(_,{input},context){
+      return  context.models.Pet.findOne(input)
+    },
+    user(_,__,context){
+      return  context.models.User.findOne()
     }
   },
-  User: {
+  Mutation: {
+    newPet(_,{input},context){
+      return context.models.Pet.create(input)
+    }
+  },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
     
-  }
+  // }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -3,8 +3,49 @@ const { gql } = require('apollo-server')
 /**
  * Type Definitions for our Schema using the SDL.
  */
+
+
+
+ // using type to create the Type schema blocks
+ //then using query to define a operation name with the right schema as its value
 const typeDefs = gql`
-  
+  	type User{ 
+  		id:ID!
+  		username:String!
+  	}
+
+  		type Pet{
+  			id:ID!
+  			createdAt:String
+  			name:String!
+  			type:String!
+  	}
+
+
+  				input petInput{
+  					name:String
+  					type:String
+  				}
+
+
+  				input singlePetInput{
+  					id:ID
+  				}
+
+  				input newPetInput{
+  					name:String!
+  					type:String!
+  				}
+
+  	type Query{
+  		user:User!
+  		pets(input:petInput):[Pet]!
+  		apet(input:singlePetInput):Pet
+  	}
+
+  	type Mutation{
+  		newPet(input:newPetInput):Pet!
+  	}
 
 `;
 
